4bfc5fe799e6559f767200f6c26d3318ecadbdb6
martin sneath <martinaneath@gmail.com>
2014-10-09 11:45:17 -0400

    all done, maybe

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+# plot 1 - before I added stringsAsFactors=F to the 
+# read.csv
 c=as.numeric(as.character(a[,3]))
 png("plot1.png")
 hist (c,col="Red",main=paste("Global Active Power"),xlab="Global Active Power (kilowatts)",ylab="Frequency")
---
+++ plot4.R
@@ -0,0 +1,11 @@
+# plot 4
+png("plot4.png")
+par (mfrow=c(2,2))
+plot(f,c,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+plot(f,a$Voltage,type="l",xlab="datetime",ylab="Voltage")
+plot(f,a$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
+lines(f,a$Sub_metering_2,type="l",col="Red")
+lines(f,a$Sub_metering_3,type="l",col="Blue")
+legend ("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("Black","Red","Blue"),lwd=1)
+plot(f,a$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
+dev.off()
